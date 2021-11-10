fc4267b7c9eac6f2da543ad7c4bf3c2cf74f5317
Karthickdev <46392745+Karthickdev@users.noreply.github.com>
2021-11-10 19:02:31 +0530

    'imageFile-upload-added'

--- src/app/scanreturns/scanreturns.page.ts
+++ src/app/scanreturns/scanreturns.page.ts
@@ -175,8 +175,8 @@ export class ScanreturnsPage implements OnInit {
     this.itemReadOnly = true;
     this.enableSaveBtn = false;
     this.enableTakePhoto = false;
-    this.photoType = [{ "typeName": "Return Label", "img": "../../assets/default-thumbnail.jpg" }, { "typeName": "SKU", "img": "../../assets/default-thumbnail.jpg" },
-    { "typeName": "Damaged Area", "img": "../../assets/default-thumbnail.jpg" }, { "typeName": "Up front", "img": "../../assets/default-thumbnail.jpg" }]
+    this.photoType = [{ "typeName": "Return Label", "img": "../../assets/default-thumbnail.jpg", "isCaptured": false }, { "typeName": "SKU", "img": "../../assets/default-thumbnail.jpg", "isCaptured": false },
+    { "typeName": "Damaged Area", "img": "../../assets/default-thumbnail.jpg", "isCaptured": false }, { "typeName": "Up front", "img": "../../assets/default-thumbnail.jpg", "isCaptured": false }]
     if (this.isSerailScan) {
       setTimeout(() => {
         this.serial.setFocus();
@@ -345,7 +345,7 @@ export class ScanreturnsPage implements OnInit {
     
     if (this.condition == "20" || this.condition == "30") {
       this.enableTakePhoto = true
-      this.enableSaveBtn = true
+      this.enableSaveBtn = false
     } else {
       this.enableTakePhoto = false
       this.enableSaveBtn = true
@@ -354,14 +354,20 @@ export class ScanreturnsPage implements OnInit {
 
   takePicture(type) {
     this.camera.getPicture(this.cameraOptions).then((imageData) => {
-      
       var filename = imageData.substring(imageData.lastIndexOf('/')+1);
       var path =  imageData.substring(0,imageData.lastIndexOf('/')+1);
          this.file.readAsDataURL(path, filename).then(res=>{
           for(let item of this.photoType){
             if(item.typeName == type.typeName){
               item.img = res;
+              item.isCaptured = true
             }
+          }
+          let checkCaptured = this.photoType.filter(i => i.isCaptured == false)
+          if(checkCaptured.length > 0){
+            this.enableSaveBtn = false
+          }else{
+            this.enableSaveBtn = true
           }
          });
       this.file.resolveLocalFilesystemUrl(imageData).then((entry: FileEntry) => {
@@ -385,152 +391,6 @@ export class ScanreturnsPage implements OnInit {
     };
     reader.readAsArrayBuffer(file);
   }
-
-  async takePhoto(type) {
-    //var tempImage = await this.camera.getPicture(this.cameraOptions);
-    // var tempFilename = tempImage.substr(tempImage.lastIndexOf('/') + 1);
-    // var tempBaseFilesystemPath = tempImage.substr(0, tempImage.lastIndexOf('/') + 1);
-    // var newBaseFilesystemPath = this.file.dataDirectory;
-    // await this.file.copyFile(tempBaseFilesystemPath, tempFilename, newBaseFilesystemPath, tempFilename);
-    // var storedPhoto = newBaseFilesystemPath + tempFilename;
-
-    // if(type.typeName == 'Return Label'){
-    //   this.returnLabelPhoto = imageFile
-    // }else if(type.typeName == 'SKU'){
-    //   this.skuPhoto = imageFile
-    // }else if(type.typeName == 'Damaged Area'){
-    //   this.damagedAreaPhoto = imageFile
-    // }else{
-    //   this.upFrontPhoto = imageFile
-    // }
-
-    // this.returnImages.push(tempImage);
-    // this.enableSaveBtn = true
-
-    
-    this.camera.getPicture(this.cameraOptions).then((imageData) => {
-      let base64Image = imageData;
-      for(let item of this.photoType){
-        if(item.typeName == type.typeName){
-          item.img = 'data:image/jpeg;base64,' + base64Image
-          if(item.typeName == 'Return Label'){
-            item.isCaptured = true
-            this.returnLabelPhoto = this.dataURItoBlob(base64Image);
-            this.testAlert(this.returnLabelPhoto);
-            //this.writeFile('returnlabel', returnBlob);
-          }else if(item.typeName == 'SKU'){
-            item.isCaptured = true
-            this.skuPhoto = this.dataURItoBlob(base64Image);
-            //this.writeFile('sku', skuBlob);
-          }else if(item.typeName == 'Damaged Area'){
-            item.isCaptured = true
-            this.damagedAreaPhoto = this.dataURItoBlob(base64Image);
-            //this.writeFile('damaged', damagedBlob);
-          }else if(item.typeName == 'Up front'){
-            item.isCaptured = true
-            this.upFrontPhoto = this.dataURItoBlob(base64Image);
-           // this.writeFile('upfront', upFrontBlob);
-          }
-        }
-      }
-
-       let checkCaptured = this.photoType.filter(i => i.isCaptured == false)
-       if(checkCaptured.length > 0){
-         this.enableSaveBtn = false
-       }else{
-         this.enableSaveBtn = true
-       }
-      // if(this.returnLabelPhoto && this.skuPhoto && this.damagedAreaPhoto && this.upFrontPhoto){
-      //   this.enableSaveBtn = true
-      // }else{
-      //   this.enableSaveBtn = false
-      // }
-     }, (err) => {
-      // Handle error
-     });
-  }
-
-  dataURItoBlob(dataURI) {
-    // contentType = contentType || '';
-    // const sliceSize = 512;
-    //     var byteCharacters = atob(dataURI);
-    //     var byteArrays = [];
-
-    //     for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-    //         var slice = byteCharacters.slice(offset, offset + sliceSize);
-
-    //         var byteNumbers = new Array(slice.length);
-    //         for (var i = 0; i < slice.length; i++) {
-    //             byteNumbers[i] = slice.charCodeAt(i);
-    //         }
-
-    //         var byteArray = new Uint8Array(byteNumbers);
-
-    //         byteArrays.push(byteArray);
-    //     }
-
-    //   var blob = new Blob(byteArrays, {type: contentType});
-    //   return blob;
-
-    var byteString;
-    if (dataURI.split(',')[0].indexOf('base64') >= 0)
-      byteString = atob(dataURI.split(',')[1]);
-    else
-      byteString = unescape(dataURI.split(',')[1]);
-    // separate out the mime component
-    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
-    // write the bytes of the string to a typed array
-    var ia = new Uint8Array(byteString.length);
-    for (var i = 0; i < byteString.length; i++) {
-      ia[i] = byteString.charCodeAt(i);
-    }
-    return new Blob([ia], { type: mimeString });
-  }
-
-  writeFile(fileName, blob){
-    const writeDirectory = this.file.externalDataDirectory
-    this.file.writeFile(writeDirectory, fileName +'.jpeg', blob, {replace: true}).then(()=>{
-      if(fileName == 'returnLabel'){
-        this.returnLabelPhoto = writeDirectory + 'returnLabel.jpeg';
-      }else if(fileName == 'sku'){
-        this.skuPhoto = writeDirectory + 'sku.jpeg'
-      }else if(fileName == 'damaged'){
-        this.damagedAreaPhoto = writeDirectory + 'damaged.jpeg'
-      }else if(fileName == 'upfront'){
-        this.upFrontPhoto = writeDirectory + 'upfront.jpeg'
-      }
-    });
-  }
-
-  // if(item.typeName == 'Return Label'){
-  //   item.isCaptured = true
-  //  this.returnLabelPhoto = {
-  //    "fileName": "retunLabel",
-  //    "imageData": base64Image,
-  //    "fileExtension": "jpeg"
-  //  }
-  // }else if(item.typeName == 'SKU'){
-  //   item.isCaptured = true
-  //   this.skuPhoto = {
-  //     "fileName": "skuPhoto",
-  //     "imageData": base64Image,
-  //     "fileExtension": "jpeg"
-  //   }
-  // }else if(item.typeName == 'Damaged Area'){
-  //   item.isCaptured = true
-  //  this.damagedAreaPhoto = {
-  //   "fileName": "damagedAreaPhoto",
-  //   "imageData": base64Image,
-  //   "fileExtension": "jpeg"
-  // }
-  // }else if(item.typeName == 'Up front'){
-  //   item.isCaptured = true
-  //  this.upFrontPhoto = {
-  //   "fileName": "upFrontPhoto",
-  //   "imageData": base64Image,
-  //   "fileExtension": "jpeg"
-  // }
-  // }
 
   // async saveReturn(){
   //     let url = this.Vanityartservice.baseUrl+this.Vanityartservice.save;
@@ -626,46 +486,17 @@ export class ScanreturnsPage implements OnInit {
   sendEmailAlert(){
     this.Vanityartservice.present();
     let serialNo = this.isSerailScan ? this.serialScanning.controls['serial'].value.toUpperCase() : '';
-    // const httpOptions = {
-    //   headers: new HttpHeaders({
-    //     'Content-Type': 'multipart/form-data'
-    //   })
-    // };
-    // const data = new FormData();
-    // data.append("returnAppImages", this.returnLabelPhoto);
-    // data.append("returnAppImages", this.skuPhoto);
-    // data.append("returnAppImages", this.damagedAreaPhoto);
-    // data.append("returnAppImages", this.upFrontPhoto);
-    
-   
-
     let url = this.Vanityartservice.baseUrl + this.Vanityartservice.sendEmail+this.respData['purchaseOrderNumber']+'/'+this.respData['itemUpc']+'/'+serialNo+
     '/'+parseInt(this.condition)+'/'+this.warehouse+'/'+this.userId+'/'+this.respData['isOpalOrder']+'/'+this.respData['isVanityArtOrder']+'/'+this.usertype;
     this.http.post(url, this.formData).subscribe(res =>{
       console.log(res);
       this.Vanityartservice.dismiss();
-      this.Vanityartservice.PresentToast("Images Uploaded", "success");
+      this.Vanityartservice.PresentToast("Return Item Images Uploaded", "success");
         this.formreset();
     },err =>{
       this.Vanityartservice.dismiss();
       this.Vanityartservice.PresentToast("Unable to reach server", "danger");
     });
-
-    this.testAlert(this.returnLabelPhoto);
-
-  }
-
-  async testAlert(name){
-    let alert = await this.alertCtrl.create({
-      message: name,
-      buttons: [
-        {
-          text: "ok",
-          handler: ()=>{}
-        }
-      ]
-    });
-    alert.present();
   }
 
 }
